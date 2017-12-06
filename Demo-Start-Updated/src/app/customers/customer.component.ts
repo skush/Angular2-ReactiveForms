30e5f2854a3526ed436b5f413d6ac7388362825f
Sergii Kushnir <skush@users.noreply.github.com>
2017-12-06 12:06:46 +0200

    Reactive forms - FormControl

--- Demo-Start-Updated/src/app/customers/customer.component.ts
+++ Demo-Start-Updated/src/app/customers/customer.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
-import { NgForm } from '@angular/forms';
-
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 import { Customer } from './customer';
 
 @Component({
     selector: 'my-signup',
     templateUrl: './app/customers/customer.component.html'
 })
-export class CustomerComponent  {
+export class CustomerComponent implements OnInit  {
+    customerForm: FormGroup;
     customer: Customer= new Customer();
 
-    save(customerForm: NgForm) {
-        console.log(customerForm.form);
-        console.log('Saved: ' + JSON.stringify(customerForm.value));
+    ngOnInit(): void {
+        this.customerForm = new FormGroup({
+            firstName: new FormControl(),
+            lastName: new FormControl(),
+            email: new FormControl(),
+            sendCatalog: new FormControl(true)
+        });
     }
+
+    populateTestData(): void {
+        this.customerForm.patchValue({
+            firstName: 'Jack',
+            ////lastName: 'Harkness',
+            email: 'jack@torchwood.com',
+            sendCatalog: false
+        });
+    }
+
+    save() {
+        console.log(this.customerForm);
+        console.log('Saved: ' + JSON.stringify(this.customerForm.value));
+    }
+
  }
